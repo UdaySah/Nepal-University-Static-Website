8f6b09d66e3509206a0581060a9429fde11ea4a8
Uday Sah <39239031+UdaySah@users.noreply.github.com>
2020-03-16 15:31:20 +0545

    Add files via upload

---
+++ view.php
@@ -0,0 +1,15 @@
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$feedback=$_POST['feedback'];
+
+echo "Name = " .$name; 
+echo "<br>";
+echo "Student Email = ".$email;
+echo "<br>";
+echo "Student Phone number= ".$phone number;
+echo "<br>";
+echo "Student Feedback= ".$feedback;
+echo "<br>";
+?>
